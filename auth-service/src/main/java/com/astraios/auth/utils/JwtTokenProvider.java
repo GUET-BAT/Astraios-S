4fdf5aa1e9cde81cd2eaf034684615d9a2181d2e
yaokong <furthersky@outlook.com>
2026-01-15 21:10:51 +0800

    feat: 实现JWKS动态获取

--- auth-service/src/main/java/com/astraios/auth/utils/JwtTokenProvider.java
+++ auth-service/src/main/java/com/astraios/auth/utils/JwtTokenProvider.java
@@ -1,73 +1,73 @@
 package com.astraios.auth.utils;
 
 
-import io.jsonwebtoken.Claims;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.KeyUse;
+import com.nimbusds.jose.jwk.RSAKey;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
-import io.jsonwebtoken.security.MacAlgorithm;
-import io.jsonwebtoken.security.WeakKeyException;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
+import java.security.KeyPair;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
 
 @Component
 @Slf4j
 public class JwtTokenProvider {
-    private static final String SECRET_KEY = "Your_Secret_Key";
-    private static final long EXPIRATION_TIME = 1000L * 60 * 60 * 24;  // 1天
+
+    private static final long EXPIRATION_TIME = 1000L * 60 * 60 * 24; // 1天
     private static final String ISSUER = "astraios";
 
-    private static SecretKey secretKey;
-    private static final MacAlgorithm ALGORITHM = Jwts.SIG.HS512; // 或 HS256
+    private static KeyPair keyPair;
+
+    private static final String KEY_ID = UUID.randomUUID().toString();
 
     @PostConstruct
     public void init() {
-        secretKey = buildKey(SECRET_KEY);
+
+        keyPair = Jwts.SIG.RS256.keyPair().build();
     }
 
-    private static SecretKey buildKey(String secret) {
-        try {
-
-            byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
-            // 如果长度不够，自动扩展
-            if (keyBytes.length < ALGORITHM.key().build().getEncoded().length) {
-                byte[] newKey = new byte[64];
-                System.arraycopy(keyBytes, 0, newKey, 0, Math.min(keyBytes.length, 64));
-                keyBytes = newKey;
-            }
-            return Keys.hmacShaKeyFor(keyBytes);
-        } catch (WeakKeyException e) {
-            throw new IllegalArgumentException("JWT secret key too weak. Must be at least 32 bytes for HS256 / 64 bytes for HS512", e);
-        }
-    }
 
     public static String generateToken(String uid) {
         Date now = new Date();
         Date expiration = new Date(now.getTime() + EXPIRATION_TIME);
 
         return Jwts.builder()
+                .header().keyId(KEY_ID).and()
                 .subject(uid)
                 .issuer(ISSUER)
                 .issuedAt(now)
                 .expiration(expiration)
-                .signWith(secretKey, ALGORITHM)
+                .signWith(keyPair.getPrivate(), Jwts.SIG.RS256)
                 .compact();
     }
 
-    public static String parseTokenForUserId(String token) {
+    public String parseTokenForUserId(String token) {
         return Jwts.parser()
-                .verifyWith(secretKey)
+                .verifyWith(keyPair.getPublic())
                 .build()
                 .parseSignedClaims(token)
                 .getPayload()
                 .getSubject();
+    }
+
+
+    public Map<String, Object> getJwkSet() {
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+
+        RSAKey key = new RSAKey.Builder(publicKey)
+                .keyUse(KeyUse.SIGNATURE)
+                .algorithm(JWSAlgorithm.RS256)
+                .keyID(KEY_ID)
+                .build();
+
+        return new JWKSet(key).toJSONObject();
     }
 }
