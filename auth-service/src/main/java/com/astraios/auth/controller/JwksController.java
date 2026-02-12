b550e90af1b55cc4464da2f19835f08966d63046
furtherSky <furthersky@outlook.com>
2026-02-12 19:23:57 +0800

    refractor:清除不需要的依赖

--- auth-service/src/main/java/com/astraios/auth/controller/JwksController.java
+++ auth-service/src/main/java/com/astraios/auth/controller/JwksController.java
@@ -1,12 +1,8 @@
 package com.astraios.auth.controller;
-import com.astraios.auth.service.AuthService;
 import com.astraios.auth.utils.JwtTokenProvider;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
